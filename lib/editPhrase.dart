7f2836fb03e73f0e53f44262eed60b1a39d56d51
Esther Lozano <esther@seedtag.com>
2018-12-20 16:10:35 +0100

    List categories for editing [WIP]

--- lib/editPhrase.dart
+++ lib/editPhrase.dart
@@ -1,16 +1,27 @@
 import 'package:conversando/commons.dart';
+import 'package:conversando/context.dart';
 import 'package:flutter/material.dart';
 
 class PhraseEditorWidget extends StatelessWidget {
   @override
   Widget build(context) {
+    final TextContextWidgetState tc = TextContextWidget.of(context);
+
     return new Scaffold(
       appBar: new AppBar(
         title: Text("Mis frases"),
         actions: <Widget>[
           new ActionBarButtonWidget("AÑADIR", () { Navigator.pop(context);})
-    ]),
-      body: Column(children: [])
-    );
-  }
+        ]
+      ),
+      body:
+      Column(
+        children: [
+//          Expanded(child: Text("Mis categorías")),
+          ListView(
+            children: tc.getCategories().map((Category category) {
+              return ListTile(title: Text(category.text));
+            }).toList()
+        )]
+    ));}
 }
